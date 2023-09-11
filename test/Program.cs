af83b55e68847a6e8ad0897920f0b8047509dea5
Melodie Wang <myw4072@rit.edu>
2023-09-10 21:01:14 -0400

    mandelbrot alterations

---
+++ test/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace test
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int number = 2;
+            switch (number)
+            {
+                case 1:
+                    Console.WriteLine("Just");
+                    break;
+                case 2:
+                    goto default;
+                    
+                case 4:
+                    Console.WriteLine("an");
+                    break;
+
+                default:
+                    Console.WriteLine("example");
+                    break;
+            }
+
+
+        }
+    }
+}
