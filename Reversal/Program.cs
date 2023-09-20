585f866ec5eee7efd7b0f1ecdc27370e58f000e1
Melodie Wang <myw4072@rit.edu>
2023-09-20 10:53:53 -0400

    reverse user input

---
+++ Reversal/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Reversal
+{
+    // Class: Program
+    // Author: Melodie Wang
+    // Purpose: reversing user input
+    // Restrictions: None
+    internal class Program
+    {
+        // Method: Main
+        // Purpose: takes string input, breakes down into characters and outputs backwards
+        // Restrictions: None
+        static void Main(string[] args)
+        {
+            string userInput;
+
+            Console.WriteLine("Enter a word");
+            userInput = Console.ReadLine();
+
+            char[] characters = userInput.ToCharArray(0,userInput.Length);
+
+            for(int i = characters.Length - 1; i >= 0; i--)
+            {
+                Console.Write(characters[i]);
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
