ddbd042fe5c68b5dab9302932cf1da9b1f399a65
Melodie Wang <myw4072@rit.edu>
2023-09-20 21:12:45 -0400

    ""

---
+++ DoubleQuotes/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DoubleQuotes
+{
+    // Class: Program
+    // Author: Melodie Wang
+    // Purpose: surrounds each word in ""
+    // Restrictions: None
+    internal class Program
+    {
+        // Method: Main
+        // Purpose: takes string input, adds "" around each word
+        // Restrictions: None
+        static void Main(string[] args)
+        {
+            string result = "";
+
+            Console.WriteLine("Input a sentance");
+            string userInput = Console.ReadLine();
+
+            // split the input into separate words
+            string[] words = userInput.Split(' ');
+
+            foreach (string word in words)
+            {
+                if (word == words[0])// not needed? || word == "." || word == "," || word == "?" || word == "!"
+                {
+                    result += ("\""+word+ "\"");
+                }
+                else
+                {
+                    result += ("\" " + word + "\"");
+                }
+            }
+            Console.WriteLine(result);
+        }
+    }
+}
