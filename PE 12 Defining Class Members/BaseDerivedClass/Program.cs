1650dd8993674d1b42f5d1ec44ef23acbbaf52a2
Melodie Wang <myw4072@rit.edu>
2023-10-13 22:24:38 -0400

    needs more work

---
+++ PE 12 Defining Class Members/BaseDerivedClass/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BaseDerivedClass
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+
+    public class MyClass
+    {
+        private string myString;
+
+        public virtual string GetString()
+        {
+            //get
+            //{
+                return myString;
+            //}
+            //set
+            //{
+            //    myString = value;
+            //}
+        }
+    }
+
+    public class MyDerivedClass : MyClass
+    {
+        public MyDerivedClass(string initialValue) : base(initialValue)
+        {
+        }
+
+        public override string GetString()
+        {
+            string baseString = base.GetString(); // Call the base class's implementation
+            return baseString + " (output from the derived class)";
+        }
+    }
+}
+
