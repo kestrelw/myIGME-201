52988569cf7b68d472e1025ff80bd309acbacd2a
kestrelw <mail4melodie@gmail.com>
2023-12-05 01:55:59 -0500

    check

--- Unit 3 Test/UserInterfaceFromHell/Form1.Designer.cs
+++ Unit 3 Test/UserInterfaceFromHell/Form1.Designer.cs
@@ -28,13 +28,153 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(231, 273);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(268, 20);
+            this.textBox1.TabIndex = 2;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(512, 266);
+            this.button1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(39, 34);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "->";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Prequel Demo", 36F, System.Drawing.FontStyle.Bold);
+            this.label1.ForeColor = System.Drawing.Color.DarkRed;
+            this.label1.Location = new System.Drawing.Point(52, 52);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(615, 120);
+            this.label1.TabIndex = 6;
+            this.label1.Text = " \r\nexperience some hell  ";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Prequel Demo", 36F, System.Drawing.FontStyle.Bold);
+            this.label3.ForeColor = System.Drawing.SystemColors.ControlLight;
+            this.label3.Location = new System.Drawing.Point(171, 175);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(406, 60);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "good puzzles?";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("Prequel Demo", 36F, System.Drawing.FontStyle.Bold);
+            this.label2.ForeColor = System.Drawing.SystemColors.ControlLight;
+            this.label2.Location = new System.Drawing.Point(627, 112);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(76, 60);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "-a";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(229, 256);
+            this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(177, 13);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Please enter your first and last name";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Prequel Demo", 36F, System.Drawing.FontStyle.Bold);
+            this.label5.ForeColor = System.Drawing.Color.DarkRed;
+            this.label5.Location = new System.Drawing.Point(250, 52);
+            this.label5.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(249, 60);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "Ready to";
+            this.label5.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.ForeColor = System.Drawing.Color.DarkRed;
+            this.label6.Location = new System.Drawing.Point(187, 323);
+            this.label6.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(390, 13);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "**Submit button giving you some trouble? Maybe there\'s a way to Lock it in place." +
+    "";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.ForeColor = System.Drawing.Color.DarkRed;
+            this.label7.Location = new System.Drawing.Point(307, 308);
+            this.label7.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(113, 13);
+            this.label7.TabIndex = 12;
+            this.label7.Text = "*Must contain a space";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(759, 361);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
     }
 }
 
--- Unit 3 Test/UserInterfaceFromHell/Form2.cs
+++ Unit 3 Test/UserInterfaceFromHell/Form2.cs
@@ -2,19 +2,325 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics;
 using System.Drawing;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Xml.Serialization;
 
 namespace UserInterfaceFromHell
 {
     public partial class Form2 : Form
     {
+        List<RadioButton> radioButtonList = new List<RadioButton>();
+        List<TextBox> textBoxList = new List<System.Windows.Forms.TextBox>();
+        List<Label> labelList = new List<Label>();
+        ErrorProvider errorProvider = new ErrorProvider();
+        //System.Windows.Forms.Timer myTimer = new System.Windows.Forms.Timer();
+        Stopwatch stopWatch = new Stopwatch();
+        static public string timeTaken;
+
         public Form2()
         {
             InitializeComponent();
+            try
+            {
+                // Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 6.2; WOW64; Trident / 7.0; .NET4.0C; .NET4.0E; .NET CLR 2.0.50727; .NET CLR 3.0.30729; .NET CLR 3.5.30729; wbx 1.0.0)
+                Microsoft.Win32.RegistryKey key = Microsoft.Win32.Registry.LocalMachine.OpenSubKey(
+                    @"SOFTWARE\\WOW6432Node\\Microsoft\\Internet Explorer\\MAIN\\FeatureControl\\FEATURE_BROWSER_EMULATION",
+                    true);
+                key.SetValue(Application.ExecutablePath.Replace(Application.StartupPath + "\\", ""), 12001, Microsoft.Win32.RegistryValueKind.DWord);
+                key.Close();
+            }
+            catch
+            {
+
+            }
+            CreateLists();
+            DeactivateAll();
+
+            button2.Visible = false;
+
+            for (int i = 0; i < 5; i++)
+            {
+                textBoxList[i].MouseEnter += new EventHandler(this.HighlightTextBox__MouseEnter);
+                textBoxList[i].MouseLeave += new EventHandler(this.HighlightTextBox__MouseLeave);
+                labelList[i].MouseEnter += new EventHandler(this.HighlightLabel__MouseEnter);
+                labelList[i].MouseLeave += new EventHandler(this.HighlightLabel__MouseLeave);
+            }
+            for (int i = 0; i < radioButtonList.Count; i++)
+            {
+                radioButtonList[i].CheckedChanged += new EventHandler(this.RadioButtonList__CheckedChanged);
+            }
+
+            this.button1.Click += new EventHandler(this.CloseButton__Clicked);
+            this.button2.Click += new EventHandler(this.CheckButton__Clicked);
+            
+
         }
+
+        private void CreateLists()//the lists to make life easier
+        {
+            radioButtonList.Add(this.radioButton1);
+            radioButtonList.Add(this.radioButton2);
+            radioButtonList.Add(this.radioButton3);
+            radioButtonList.Add(this.radioButton4);
+            radioButtonList.Add(this.radioButton5);
+            radioButtonList.Add(this.radioButton6);
+            radioButtonList.Add(this.radioButton7);
+            radioButtonList.Add(this.radioButton8);
+            radioButtonList.Add(this.radioButton9);
+            radioButtonList.Add(this.radioButton10);
+            radioButtonList.Add(this.radioButton11);
+            radioButtonList.Add(this.radioButton12);
+            radioButtonList.Add(this.radioButton13);
+            radioButtonList.Add(this.radioButton14);
+            //radioButtonList[0].
+
+            textBoxList.Add(this.textBox1);
+            textBoxList.Add(this.textBox2);
+            textBoxList.Add(this.textBox3);
+            textBoxList.Add(this.textBox4);
+            textBoxList.Add(this.textBox5);
+            //textBoxList[0].
+
+            labelList.Add(this.label1);
+            labelList.Add(this.label2);
+            labelList.Add(this.label3);
+            labelList.Add(this.label4);
+            labelList.Add(this.label5);
+        }
+
+        private void DeactivateAll()
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                textBoxList[i].Enabled = false;
+                labelList[i].Enabled = false;
+            }
+            for (int i = 0; i < radioButtonList.Count; i++)
+            {
+                radioButtonList[i].Enabled = false;
+            }
+        }
+        private void ActivateAll()
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                textBoxList[i].Enabled = true;
+                labelList[i].Enabled = true;
+            }
+            for (int i = 0; i < radioButtonList.Count; i++)
+            {
+                radioButtonList[i].Enabled = true;
+            }
+        }
+
+        private void RadioButtonList__CheckedChanged(object sender, EventArgs e)
+        {
+            RadioButton rb = (RadioButton)sender;
+
+            // if the radio button is checked
+            if (rb.Checked)
+            {
+                if (rb == radioButtonList[0])
+                {
+                    Blackout();
+                    textBoxList[0].BackColor = Color.Ivory;
+                    textBoxList[0].BorderStyle = BorderStyle.FixedSingle;
+                    labelList[0].ForeColor = Color.Ivory;
+
+                }
+                else if (rb == radioButtonList[1])
+                {
+                    Blackout();
+                    textBoxList[1].BackColor = Color.Ivory;
+                    textBoxList[1].BorderStyle = BorderStyle.FixedSingle;
+                    labelList[1].ForeColor = Color.Ivory;
+                }
+                else if (rb == radioButtonList[2])
+                {
+                    Blackout();
+                    textBoxList[2].BackColor = Color.Ivory;
+                    textBoxList[2].BorderStyle = BorderStyle.FixedSingle;
+                    labelList[2].ForeColor = Color.Ivory;
+                }
+                else if (rb == radioButtonList[3])
+                {
+                    Blackout();
+                    textBoxList[3].BackColor = Color.Ivory;
+                    textBoxList[3].BorderStyle = BorderStyle.FixedSingle;
+                    labelList[3].ForeColor = Color.Ivory;
+                }
+                else if (rb == radioButtonList[4])
+                {
+                    Blackout();
+                    textBoxList[4].BackColor = Color.Ivory;
+                    textBoxList[4].BorderStyle = BorderStyle.FixedSingle;
+                    labelList[4].ForeColor = Color.Ivory;
+                }
+                else
+                {
+                    Blackout();
+                }
+
+
+            }
+        }
+
+        public void TxtBoxEmpty__Validating(object sender, CancelEventArgs e)
+        {
+            TextBox tb = (TextBox)sender;
+
+            if (tb.Text.Length == 0)
+            {
+                this.errorProvider.SetError(tb, "This field cannot be empty.");
+                e.Cancel = true;
+            }
+            else
+            {
+                this.errorProvider.SetError(tb, null);
+                e.Cancel = false;
+            }
+
+
+        }
+
+        public void TxtBox__TextChanged(object sender, EventArgs e)
+        {
+            TextBox tb = (TextBox)sender;
+
+            this.errorProvider.SetError(tb, null);
+
+            for (int i = 0; i < 5; i++)
+            {
+                if (textBoxList[i].Text.Length > 0)
+                {
+                    continue;
+                }
+                else
+                {
+                    return;
+                }
+            }
+
+            button2.Visible = true;
+        }
+
+        private void Blackout()
+        {
+            this.BackColor = Color.Black;
+
+            for (int i = 0; i < 5; i++)
+            {
+                textBoxList[i].BackColor = Color.Black;
+                textBoxList[i].BorderStyle = BorderStyle.None;
+                labelList[i].ForeColor = Color.Black;
+
+
+            }
+        }
+
+        private void BlackoutPopup()
+        {
+            groupBox1.Visible = true;
+            groupBox1.BackColor = Color.DarkGray;
+            groupBox1.ForeColor = Color.DarkRed;
+            label6.Text = "It appears we are having some power\ndifficulties. Why don’t you try and feel around for\na few moments to try and get your bearings.\n\n Bzz Bzzt";
+        }
+
+        private void HighlightTextBox__MouseEnter(object sender, EventArgs e)
+        {
+            TextBox textBox = (TextBox)sender;
+            textBox.BackColor = Color.Ivory;
+            textBox.BorderStyle = BorderStyle.FixedSingle;
+        }
+        private void HighlightTextBox__MouseLeave(object sender, EventArgs e)
+        {
+            TextBox textBox = (TextBox)sender;
+            textBox.BackColor = Color.Black;
+            textBox.BorderStyle = BorderStyle.None;
+        }
+
+        private void HighlightLabel__MouseEnter(object sender, EventArgs e)
+        {
+            Label label = (Label)sender;
+            label.ForeColor = Color.Ivory;
+        }
+        private void HighlightLabel__MouseLeave(object sender, EventArgs e)
+        {
+            Label label = (Label)sender;
+            label.ForeColor = Color.Black;
+        }
+
+        private void CloseButton__Clicked(object sender, EventArgs e)
+        {
+            groupBox1.Visible = false;
+            for (int i = 0; i < 5; i++)
+            {
+                textBoxList[i].Validating += new CancelEventHandler(TxtBoxEmpty__Validating);
+                textBoxList[i].TextChanged += new EventHandler(TxtBox__TextChanged);
+                
+            }
+
+            //Thread.Sleep(3000);//causing groupbox visibility remain errors
+            Blackout();
+
+            if (textBoxList[0].Enabled == false)
+            {
+                BlackoutPopup();
+            }
+            ActivateAll();
+
+            stopWatch.Start();
+
+
+        }
+
+        private void CheckButton__Clicked(object sender, EventArgs e)
+        {
+            ValidateAll();
+        }
+        private void ValidateAll()
+        {
+            bool incorrect = false;
+
+            for (int i = 0; i < 5; i++)
+            {
+                if (textBoxList[i].Text.ToLower() == (string)textBoxList[i].Tag)
+                {
+                    continue;
+                }
+                else
+                {
+                    this.errorProvider.SetError(textBoxList[i], "This answer is incorrect.");
+                    incorrect = true;
+                }
+            }
+
+            
+            if (incorrect)
+            {
+                return;
+            }
+            
+
+            //if none wrong then reach this point
+            stopWatch.Stop();
+            TimeSpan ts = stopWatch.Elapsed;
+            timeTaken =  String.Format("{0:00}:{1:00}:{2:00}.{3:00}",
+            ts.Hours, ts.Minutes, ts.Seconds,
+            ts.Milliseconds / 10);
+
+            Form3 form3 = new Form3();
+            form3.Show();
+            this.Hide();
+        }
+        
     }
 }
+
+
